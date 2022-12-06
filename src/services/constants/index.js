b414b6427246a81dfbf98aae7ff2918a597cb1cb
Abdul rehman <abdulrehman.dev@techxpert.io>
2022-12-06 19:32:01 +0500

    Responsiveness issue fix

--- src/services/constants/index.js
+++ src/services/constants/index.js
@@ -1,5 +1,8 @@
-import { Dimensions } from "react-native"
-const { width, height } = Dimensions.get('window');
+import { Dimensions, PixelRatio } from 'react-native';
+const WINDOW_WIDTH = Dimensions.get('window').width;
+const WINDOW_HEIGHT = Dimensions.get('window').height;
+const SCREEN_WIDTH = Dimensions.get('screen').width;
+const SCREEN_HEIGHT = Dimensions.get('screen').height;
 
 export const storageKey = {}
 
@@ -34,5 +37,34 @@ export const loaderStyles = {
 }
 
 
-export const wp = (p) => width * (p / 100);
-export const hp = (p) => height * (p / 100);
+export const wp = (p) => WINDOW_WIDTH * (p / 100);
+export const hp = (p) => WINDOW_HEIGHT * (p / 100);
+export { WINDOW_HEIGHT, WINDOW_WIDTH, SCREEN_HEIGHT, SCREEN_WIDTH };
+
+const widthBaseScale = SCREEN_WIDTH / 375;
+const heightBaseScale = SCREEN_HEIGHT / 812;
+
+function normalize(size, based = 'width') {
+    const newSize =
+        based === 'height' ? size * heightBaseScale : size * widthBaseScale;
+    return Math.round(PixelRatio.roundToNearestPixel(newSize));
+}
+const widthPixel = size => {
+    return normalize(size, 'width');
+};
+const heightPixel = size => {
+    return normalize(size, 'height');
+};
+const fontPixel = size => {
+    return heightPixel(size);
+};
+
+export {
+    widthPixel,
+    heightPixel,
+    fontPixel,
+
+};
+
+
+
